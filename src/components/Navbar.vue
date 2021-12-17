7b55923898c6e62f96588e001e8c43283482dac3
Dimas Andhika Diputra <dimasandhikadiputra@gmail.com>
2021-12-17 11:14:41 +0700

    Initial Commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,50 @@
+<template>
+	<nav class="navbar navbar-expand-lg navbar-dark">
+		<div class="container">
+			<router-link class="navbar-brand" to="/">@dimasandhk</router-link>
+			<button
+				class="navbar-toggler"
+				type="button"
+				data-toggle="collapse"
+				data-target="#navbarNavAltMarkup"
+			>
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+				<div class="navbar-nav ml-auto">
+					<router-link class="nav-link" to="/">Home</router-link>
+					<a class="nav-link" href="#">Github</a>
+					<a class="nav-link" href="#">Project</a>
+				</div>
+			</div>
+		</div>
+	</nav>
+</template>
+
+<script>
+export default {
+	name: "Navbar"
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/scss/var.scss";
+.navbar {
+	.nav-link {
+		padding-right: 30px;
+	}
+	.navbar-brand {
+		font-weight: 600;
+		background-color: $bg !important;
+	}
+	padding-top: 12px;
+	padding-bottom: 12px;
+	border-bottom: $border 2px solid;
+}
+</style>
+
+<style lang="scss">
+.router-link-active {
+	color: #fff !important;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="home container">
+		<div class="intro">
+			<h1>Dimas Andhika</h1>
+			<div class="row">
+				<div class="col-12 col-md-10 col-lg-8">
+					<p class="desc">
+						Hi! I am Dimas Andhika, I am a 15 year old student who is interested and has a hobby in
+						programming. All my repos on github are just an implementations of what I've learned so
+						far.
+					</p>
+				</div>
+			</div>
+		</div>
+		<div class="contact">
+			<h1>Get In Touch</h1>
+		</div>
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+	name: "Home"
+};
 </script>
+
+<style lang="scss" scoped>
+.home {
+	padding-top: 40px;
+}
+
+.intro {
+	.desc {
+		font-size: 20px;
+	}
+}
+
+@media only screen and (max-width: 768px) {
+	.home {
+		padding-top: 30px;
+	}
+	.intro {
+		h1 {
+			font-size: 36px;
+		}
+		.desc {
+			font-size: 16px;
+		}
+	}
+}
+</style>
